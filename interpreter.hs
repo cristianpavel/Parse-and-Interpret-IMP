788a54da13b018ce68d666324030bff503f05f91
Cristian Pavel <pcristian97@googlemail.com>
2018-11-18 12:23:02 +0200

    added do syntax in monad

--- interpreter.hs
+++ interpreter.hs
@@ -1,10 +1,11 @@
 import Data.Char
 import Data.Map
 
+type State = Map String Integer
 data AExpr = Var String | AVal Integer | Plus AExpr AExpr | AEncaps AExpr deriving Show
 data BExpr = BTrue | BFalse | And BExpr BExpr | Greater AExpr AExpr | BEncaps BExpr deriving Show
 data Stmt = Void | While BExpr Stmt | Equal String AExpr | If BExpr Stmt Stmt | Colon Stmt Stmt deriving Show
-data Interpreter a = Interpreter {eval :: (Map String Integer) -> [((Map String Integer), a)]} 
+data Interpreter a = Interpreter {eval :: State -> [(State, a)]} 
 
 instance Functor (Interpreter) where
     fmap f p = pure f <*> p
@@ -22,14 +23,23 @@ instance Monad (Interpreter) where
 interpretAExpr :: AExpr -> Interpreter Integer
 interpretAExpr (Var x) = getValue x
 interpretAExpr (AVal  x) = return (x)
-interpretAExpr (Plus l r) = (interpretAExpr l) >>= (\x -> interpretAExpr r >>= (\y -> return (x + y)))
+interpretAExpr (Plus l r) = do left <- interpretAExpr l
+                               right <- interpretAExpr r
+                               return (left + right)
 
 
 interpretBExpr :: BExpr -> Interpreter Bool
 interpretBExpr (BTrue) = return (True)
 interpretBExpr (BFalse) = return (False)
-interpretBExpr (And l r) = (interpretBExpr l) >>= (\s -> if s == False then return False else interpretBExpr r >>= (\s2 -> return (s && s2)))
-interpretBExpr (Greater l r) = interpretAExpr l >>= (\x -> interpretAExpr r >>= (\y -> return (x > y)))
+interpretBExpr (And l r) = do left <- interpretBExpr l
+                              if (left == False) then
+                                     return False
+                              else
+                                    do right <- interpretBExpr r
+                                       return (left && right)
+interpretBExpr (Greater l r) = do left <- interpretAExpr l
+                                  right <- interpretAExpr r
+                                  return (left > right) 
 
 updateValue :: (String, Integer) -> Interpreter Integer
 updateValue (k,v) = Interpreter (\s -> let s1 = insert k v s in [(s1, 0)])
@@ -39,8 +49,21 @@ getValue k = Interpreter (\s -> if member k s then [(s, s ! k)] else [])
 
 interpretStmt :: Stmt -> Interpreter Integer
 interpretStmt Void = return 0
-interpretStmt (Equal var asg) = interpretAExpr asg >>= (\s -> updateValue (var, s))
-interpretStmt x@(While bexpr stm) = interpretBExpr bexpr >>= (\b -> if b then interpretStmt stm >>= (\s -> interpretStmt x) else return 0)
-interpretStmt (If bexpr t f) = interpretBExpr bexpr >>= (\b -> if b then interpretStmt t else interpretStmt f)
-interpretStmt (Colon l r) = interpretStmt l >>= (\x -> interpretStmt r)
+interpretStmt (Equal var right) = do rvalue <- interpretAExpr right
+                                     updateValue (var, rvalue)
+interpretStmt x@(While bexpr stm) = do condition <- interpretBExpr bexpr
+                                       if condition then
+                                          do interpretStmt stm
+                                             interpretStmt x
+                                       else
+                                           return 0
+
+interpretStmt (If bexpr t f) = do condition <- interpretBExpr bexpr
+                                  if condition then
+                                     do interpretStmt t
+                                  else
+                                     do interpretStmt f
+
+interpretStmt (Colon l r) = do interpretStmt l
+                               interpretStmt r
 
