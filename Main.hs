7481aa67ca8c4eb27421f9dbc6c8bbf10f097158
Cristian Pavel <pcristian97@googlemail.com>
2018-11-18 17:31:12 +0200

    now run with arguments

--- Main.hs
+++ Main.hs
@@ -2,10 +2,12 @@ import InterpreterImp
 import ParserImp
 import System.IO
 import Data.Map
+import System.Environment
 
 main :: IO ()
-main = do fileName <- getLine
-          file <- readFile fileName
-          print (eval (interpretStmt (snd (head (apply parseProg file)))) empty)
-
+main = do args <- getArgs
+          case args of
+             [fileName] -> do file <- readFile fileName
+                              print (eval (interpretStmt (snd (head (apply parseProg file)))) empty)
+             _ -> putStrLn "Wrong number of args"
           
